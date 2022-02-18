b5650aefd24bf2114d092cc79113ad09a4fec7d5
Marco Antonio da Silva Francisco <marcospatton@hotmail.com>
2022-02-18 11:20:25 -0300

    AdicionadoExercicios em C
    AdicionadoExercicios em C

---
+++ IfeElse.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int A, B, X;
+    A=10;
+    B=5;
+
+    if(A !=B){
+        printf("\nNumeros diferentes");
+        X=1;
+    }else{
+    printf("\nNumeros iguais");
+    }
+return 0;
+}
+
---
+++ novo.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int A, B;
+    printf("\n Digite o valor 1: \t");
+    scanf("%d", &A);
+
+    printf("\ Digite o valor 2: \t");
+    scanf("%d", &B);
+
+    printf("\n valor 2: \t %d", B);
+
+return 0;
+}
