45c383541e0381a4900751921b46161ba1dcdea0
Marco Antonio da Silva Francisco <marcospatton@hotmail.com>
2022-02-18 11:22:18 -0300

    Adicionado exercicio If
    Adicionado If

---
+++ IfEElse2.c
@@ -0,0 +1,18 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int A, B, X;
+    A=10;
+    B=5;
+
+    if(A !=B){
+       if(A>B){
+        printf("OK");
+       }
+       }
+return 0;
+}
+
